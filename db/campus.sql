47f35ddcba48f984363ff05d042e26817b01fc2c
Er Anees Akbar <99726488+eranees@users.noreply.github.com>
2023-08-24 13:24:20 +0530

    Added Database

--- db/campus.sql
+++ db/campus.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 01, 2023 at 09:25 PM
+-- Generation Time: Aug 24, 2023 at 09:52 AM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 7.4.27
 
@@ -62,8 +62,8 @@ INSERT INTO `admin_login` (`id`, `name`, `email`, `password`) VALUES
 
 CREATE TABLE `applications` (
   `id` bigint(20) NOT NULL,
-  `user_id` varchar(50) DEFAULT NULL,
-  `job_id` varchar(50) DEFAULT NULL,
+  `user_id` bigint(50) DEFAULT NULL,
+  `job_id` bigint(50) DEFAULT NULL,
   `applied_on` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -72,13 +72,13 @@ CREATE TABLE `applications` (
 --
 
 INSERT INTO `applications` (`id`, `user_id`, `job_id`, `applied_on`) VALUES
-(6, '32', '11', '2023-06-16 05:15:23'),
-(7, '32', '11', '2023-06-17 10:40:53'),
-(8, '32', '11', '2023-06-25 16:03:27'),
-(9, '36', '11', '2023-07-04 05:31:48'),
-(10, '32', '12', '2023-08-01 17:59:48'),
-(11, '37', '11', '2023-08-01 18:11:43'),
-(12, '37', '12', '2023-08-01 18:19:43');
+(6, 32, 11, '2023-06-16 05:15:23'),
+(7, 32, 11, '2023-06-17 10:40:53'),
+(8, 32, 11, '2023-06-25 16:03:27'),
+(9, 36, 11, '2023-07-04 05:31:48'),
+(10, 32, 12, '2023-08-01 17:59:48'),
+(11, 37, 11, '2023-08-01 18:11:43'),
+(12, 37, 12, '2023-08-01 18:19:43');
 
 -- --------------------------------------------------------
 
@@ -125,7 +125,7 @@ CREATE TABLE `company_login` (
 INSERT INTO `company_login` (`id`, `name`, `email`, `password`, `phone`, `about`, `status`) VALUES
 (9, 'code today', 'codetoday@gmail.com', 'code@123', '9596061005', 'software developement', 'Confirm'),
 (10, 'google', 'google@gmail.com', '123456', '8896401911', 'we developemet', 'Confirm'),
-(11, 'junaid', 'admin@gmail.com', 'admin', '4939483583', 'scsf', 'Pending');
+(11, 'junaid', 'admin@gmail.com', 'admin', '4939483583', 'scsf', 'Confirm');
 
 -- --------------------------------------------------------
 
@@ -135,7 +135,7 @@ INSERT INTO `company_login` (`id`, `name`, `email`, `password`, `phone`, `about`
 
 CREATE TABLE `jobs` (
   `id` bigint(20) NOT NULL,
-  `company_id` varchar(30) NOT NULL,
+  `company_id` bigint(30) NOT NULL,
   `title` varchar(50) DEFAULT NULL,
   `location` varchar(50) DEFAULT NULL,
   `job_type` varchar(50) DEFAULT NULL,
@@ -157,8 +157,8 @@ CREATE TABLE `jobs` (
 --
 
 INSERT INTO `jobs` (`id`, `company_id`, `title`, `location`, `job_type`, `skills`, `salary`, `industry`, `role`, `qualification`, `grd_marks`, `experience`, `description`, `posts`, `dop`, `approved`) VALUES
-(11, '9', 'web developer', 'rajouri', 'Permanent Job, Full Time', 'html css js bootstrap node react', '25000', '1', 'junior developer', 'mca', '75', 'Fresher', 'web developer', '15', '2023-08-01 17:54:01', 'Posted'),
-(12, '9', 'Accountant', 'Something', 'Permanent Job, Full Time', 'Imba', '50000', '2', 'Accountant', 'IMBA', '55', 'Fresher', 'Hey', '1', '2023-08-01 17:57:44', 'Posted');
+(11, 9, 'web developer', 'rajouri', 'Permanent Job, Full Time', 'html css js bootstrap node react', '25000', '1', 'junior developer', 'mca', '75', 'Fresher', 'web developer', '15', '2023-08-01 17:54:01', 'Posted'),
+(12, 9, 'Accountant', 'Something', 'Permanent Job, Full Time', 'Imba', '50000', '2', 'Accountant', 'IMBA', '55', 'Fresher', 'Hey', '1', '2023-08-01 17:57:44', 'Posted');
 
 -- --------------------------------------------------------
 
@@ -168,7 +168,7 @@ INSERT INTO `jobs` (`id`, `company_id`, `title`, `location`, `job_type`, `skills
 
 CREATE TABLE `my_details` (
   `id` bigint(20) NOT NULL,
-  `userid` varchar(20) DEFAULT NULL,
+  `userid` bigint(20) DEFAULT NULL,
   `grad` varchar(100) DEFAULT NULL,
   `college` varchar(100) DEFAULT NULL,
   `per_grad` varchar(100) DEFAULT NULL,
@@ -183,7 +183,7 @@ CREATE TABLE `my_details` (
 --
 
 INSERT INTO `my_details` (`id`, `userid`, `grad`, `college`, `per_grad`, `pg`, `un`, `per_pg`, `other`) VALUES
-(11, '32', 'BCA', 'OTHER', '69', 'MCA', 'OTHER', '85', 'c');
+(11, 32, 'BCA', 'OTHER', '69', 'MCA', 'OTHER', '85', 'c');
 
 -- --------------------------------------------------------
 
@@ -195,7 +195,7 @@ CREATE TABLE `reviews` (
   `id` bigint(20) NOT NULL,
   `review` varchar(500) DEFAULT NULL,
   `rating` varchar(20) DEFAULT NULL,
-  `user_id` varchar(20) DEFAULT NULL
+  `user_id` bigint(20) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -203,7 +203,7 @@ CREATE TABLE `reviews` (
 --
 
 INSERT INTO `reviews` (`id`, `review`, `rating`, `user_id`) VALUES
-(1, ' this is the best app', '5', '36');
+(1, ' this is the best app', '5', 36);
 
 -- --------------------------------------------------------
 
@@ -222,22 +222,20 @@ CREATE TABLE `users` (
   `about` varchar(500) DEFAULT NULL,
   `password` varchar(50) DEFAULT NULL,
   `doj` timestamp NOT NULL DEFAULT current_timestamp(),
-  `status` varchar(20) NOT NULL DEFAULT '1',
+  `status` tinyint(4) NOT NULL DEFAULT 0,
   `resume` varchar(500) NOT NULL,
-  `profile` varchar(500) NOT NULL
+  `profile` varchar(500) NOT NULL,
+  `notification` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `name`, `email`, `mobile`, `industry`, `experience`, `address`, `about`, `password`, `doj`, `status`, `resume`, `profile`) VALUES
-(32, 'TAWSEEF BASHIR', 'bhatkt71@gmail.com', '09596061005', 'Information Technology', '1', 'ganderal', 'fresher lookig for web developement', '123456', '2023-06-15 17:24:02', '1', '', ''),
-(33, 'naina ', 'naina@gmail.com', '7889640191', 'Information Technology', '2', 'srinagar', 'mern stack', '123456', '2023-06-19 04:44:57', '0', '', ''),
-(34, 'qamar rayees khan', 'qamarsir@gmail.com', '9876541323', 'Information Technology', '1', 'srinagar', 'asdfghrty', '123456', '2023-06-25 16:33:05', '0', '', ''),
-(35, 'TAWSEEF BASHIR', 'bhatkt7@gmail.com', '09596061006', 'Accounting / Finance', '1', 'fashipora tullamulla', 'asdfgh', '123456', '2023-07-04 05:09:28', '0', '', ''),
-(36, 'junaid', 'junaidnazir9292@gmail.com', '87778778765', 'Accounting / Finance', '1', 'zxcxz', 'zxcz', '123', '2023-07-04 05:19:02', '0', 'resumes/2025316331688448311paper wizard.docx', 'img/19005309291688448440code-today-srinagar (1).jpg'),
-(37, 'demo', 'demo@gmail.com', '123', '1', '', NULL, NULL, 'demo', '2023-08-01 18:11:16', '1', '', '');
+INSERT INTO `users` (`id`, `name`, `email`, `mobile`, `industry`, `experience`, `address`, `about`, `password`, `doj`, `status`, `resume`, `profile`, `notification`) VALUES
+(32, 'TAWSEEF BASHIR', 'bhatkt71@gmail.com', '09596061005', 'Information Technology', '1', 'ganderal', 'fresher lookig for web developement', '123456', '2023-06-15 17:24:02', 1, 'resumes/16959191771692808889V-Connect (DCS BGSBU).pdf', 'img/9888737281692808757anees-min.jpg', NULL),
+(33, 'naina ', 'naina@gmail.com', '7889640191', 'Information Technology', '2', 'srinagar', 'mern stack', '123456', '2023-06-19 04:44:57', 0, '', 'img/image.png', NULL),
+(38, 'Anees', 'anees@gmail.com', '9018267271', '1', '1', 'Safapora Ganderbal', 'Web Developer', '123456', '2023-08-23 17:54:10', 1, '', 'img/10425655621692816119anees-min.jpg', NULL);
 
 --
 -- Indexes for dumped tables
@@ -259,7 +257,9 @@ ALTER TABLE `admin_login`
 -- Indexes for table `applications`
 --
 ALTER TABLE `applications`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `job_id` (`job_id`);
 
 --
 -- Indexes for table `category`
@@ -271,25 +271,28 @@ ALTER TABLE `category`
 -- Indexes for table `company_login`
 --
 ALTER TABLE `company_login`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`) USING BTREE;
 
 --
 -- Indexes for table `jobs`
 --
 ALTER TABLE `jobs`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `company_id` (`company_id`);
 
 --
 -- Indexes for table `my_details`
 --
 ALTER TABLE `my_details`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `userid` (`userid`);
 
 --
 -- Indexes for table `reviews`
 --
 ALTER TABLE `reviews`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`);
 
 --
 -- Indexes for table `users`
@@ -353,7 +356,36 @@ ALTER TABLE `reviews`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `applications`
+--
+ALTER TABLE `applications`
+  ADD CONSTRAINT `applications_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
+  ADD CONSTRAINT `applications_ibfk_2` FOREIGN KEY (`job_id`) REFERENCES `jobs` (`id`);
+
+--
+-- Constraints for table `jobs`
+--
+ALTER TABLE `jobs`
+  ADD CONSTRAINT `jobs_ibfk_1` FOREIGN KEY (`company_id`) REFERENCES `company_login` (`id`);
+
+--
+-- Constraints for table `my_details`
+--
+ALTER TABLE `my_details`
+  ADD CONSTRAINT `my_details_ibfk_1` FOREIGN KEY (`userid`) REFERENCES `users` (`id`);
+
+--
+-- Constraints for table `reviews`
+--
+ALTER TABLE `reviews`
+  ADD CONSTRAINT `reviews_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
